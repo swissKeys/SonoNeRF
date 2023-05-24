4787969ef5d8b7e50dce7e64e8f0acf47db03159
swissKeys <becki_barth@yahoo.com>
2023-05-24 14:40:23 +0200

    🐛 fixed poses formation

--- helpers/recal_pos.py
+++ helpers/recal_pos.py
@@ -1,29 +1,17 @@
 import numpy as np
 
-data_processed = []
-
-with open("data/rawdata/Sononerf_Data_1/poses.txt", "r") as file:
+# Open the text file
+with open('processed_data_file.txt', 'r') as file:
     lines = file.readlines()
-    for line in lines:
-        values = line.split()  # Assuming the values are space-separated
-        if len(values) >= 9:  # Check if there are enough values in this line
-            # Ignore the first two values and keep the rest
-            data_processed.append(values[2:])
 
-# If you want to convert the values to float:
-for i in range(len(data_processed)):
-    data_processed[i] = [float(val) for val in data_processed[i]]
+# Add "0 8" to each line
+lines_with_depthbounds = [line.strip() + " 0 8\n" for line in lines]
 
-# Assume near_bound and far_bound are your determined values
-near_bound = 0
-far_bound = 8
+# Convert to NumPy array
+data = np.loadtxt(lines_with_depthbounds)
 
-# Append the depth bounds to each data line
-for i in range(len(data_processed)):
-    data_processed[i] += [near_bound, far_bound]
+# Save as CSV file
+np.savetxt('poses_bounds.csv', data, delimiter=',')
 
-# Convert the list to a numpy array
-data_processed_np = np.array(data_processed)
-
-# Save the numpy array to a file
-np.save('poses_bounds.npy', data_processed_np)
+# Save as NumPy .npy file
+np.save('poses_bounds.npy', data)
