9dd9db8a994eed15de2b87446787cb207156a7bc
Rebecca Barth <rebecca.barth@esac0111185.esa-ad.esa.int>
2023-06-12 10:05:58 +0200

    construct based on poses estimated for liver (most positive response)

--- helpers/recal_pos.py
+++ helpers/recal_pos.py
@@ -1,13 +1,7 @@
 import numpy as np
 
 # Read the CSV file
-data = np.loadtxt('data/preprocessed_data/poses_bounds_estimated.csv', delimiter=',')
-
-# Replace the last two values in each line with 10 and 297
-data[:, -2:] = 10, 297
-
-# Save as a new CSV file
-np.savetxt('poses_bounds.csv', data, delimiter=',')
+data = np.loadtxt('data/preprocessed_data/pose_bounds_old.csv', delimiter=',')
 
 # Save as a new NumPy .npy file
 np.save('poses_bounds.npy', data)
